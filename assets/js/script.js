3ee45977fc7151f943394e30e1be0aff61b90e8b
Padmashree Thakare <padmashree@Arvinds-MacBook-Pro.local>
2025-12-07 11:40:23 +0530

    theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,45 @@ lightboxLinks.forEach(link => {
   });
 });
 
-closeBtn.addEventListener('click', () => {
+closeBtn?.addEventListener('click', () => {
   popup.style.display = 'none';
 });
+
+
+// THEME SWITCHER
+const themeSelect = document.getElementById("themeSelect");
+
+  console.log("themeSelect ", themeSelect);
+
+// Load Saved Theme
+const savedTheme = localStorage.getItem("theme");
+console.log(savedTheme)
+if (savedTheme) {
+  document.documentElement.setAttribute("data-theme", savedTheme);
+  themeSelect.value = savedTheme;
+}
+
+// Change Theme on Select
+themeSelect.addEventListener("change", () => {
+  const theme = themeSelect.value;
+  console.log("theme ", theme);
+  document.documentElement.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
+});
+
+// THEME ICON TOGGLE BUTTON
+const themeToggle = document.getElementById("themeToggle");
+
+// When button clicked → toggle between light & dark
+themeToggle.addEventListener("click", () => {
+  const current = document.documentElement.getAttribute("data-theme") || "light";
+  const nextTheme = current === "light" ? "dark" : "light";
+
+  document.documentElement.setAttribute("data-theme", nextTheme);
+  localStorage.setItem("theme", nextTheme);
+});
+
+// Load correct icon according to saved theme on page load
+if (savedTheme === "dark") {
+  document.documentElement.setAttribute("data-theme", "dark");
+}
